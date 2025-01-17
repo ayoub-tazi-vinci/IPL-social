4a6e600b8aa1fd5316147d0205fb7b8aaf8cc6bb
tazi <ayoub.vinci@student.vinci.be>
2025-01-17 17:10:56 +0100

    add:when given a password with no numbers return false

--- src/app.js
+++ src/app.js
@@ -13,7 +13,9 @@ export class App{
     }
 
     passwordCheckNumber(password) {
-        return true;
+       const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+         return numbers.some(num => password.includes(num));
     }
 
 } 
